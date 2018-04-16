611f28a3c2a6c17d81122b4780e6b78b1b5d262b
Virgilius Santos <virgilius.santos@acad.pucrs.br>
2018-04-16 20:11:29 -0300

    Create project and import sample

---
+++ src/sample/Main.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("FutAges_Plano_C");
+
+        /**  scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent doubleClicked) {
+        if (doubleClicked.getClickCount() == 2){
+        stage.setFullScreen(true);
+        }
+        }
+        });
+         */
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
