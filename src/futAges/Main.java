8cf81d99752c7bcda017071ae966355576e6ed0e
Virgilius Santos <virgilius.santos@acad.pucrs.br>
2018-05-15 07:59:03 -0300

    fix path

--- src/futAges/Main.java
+++ src/futAges/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
         primaryStage = stage;
-        Parent root = FXMLLoader.load(getClass().getResource("view/fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
 
         Scene scene = new Scene(root);
 
