f44f504d267d435db91173ad5325a6b2325f65b1
Virgilius Santos <virgilius.santos@acad.pucrs.br>
2018-05-15 00:46:31 -0300

    new views e controllers

---
+++ src/futAges/controller/FXMLMainController.java
@@ -0,0 +1,55 @@
+package futAges.controller;
+
+import Main.MainDataController;
+import futAges.modal.IO.IOFiles;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXMLMainController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void handleMenuItemFileLoad() throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Load Project", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+        if (alert.getResult() == ButtonType.NO) return;
+
+        File file = IOFiles.getLoadFilePath();
+
+        MainDataController mainDataController = IOFiles.load(file, MainDataController.class);
+        if (mainDataController == null) return;
+
+        FXMLLoader loader = new FXMLLoader();
+        String path = "/futAges/view/MainPlayer.fxml";
+        loader.setLocation(FXMLMainPlayerController.class.getResource(path));
+        AnchorPane a = loader.load();
+
+        FXMLMainPlayerController mainPlayerController = loader.getController();
+        mainPlayerController.setController(mainDataController);
+
+        anchorPane.getChildren().add(a);
+        AnchorPane.setTopAnchor(a,0.0);
+        AnchorPane.setBottomAnchor(a,0.0);
+        AnchorPane.setLeftAnchor(a,0.0);
+        AnchorPane.setRightAnchor(a,0.0);
+
+    }
+
+
+}
---
+++ src/futAges/controller/FXMLMainPlayerController.java
@@ -0,0 +1,91 @@
+package futAges.controller;
+
+import Main.MainDataController;
+import futAges.modal.Util.StringFuncions;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXMLMainPlayerController implements Initializable {
+
+    private MainDataController controller;
+
+    @FXML
+    private AnchorPane anchorTable;
+    @FXML
+    private AnchorPane anchorMediaPlayer;
+
+    private FXMLTableController tableController;
+    private FXMLPlayerViewController playerViewController;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        try {
+            initializeTable();
+            initializePlayer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void initializeTable() throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        String path = "/futAges/view/TableView.fxml";
+        loader.setLocation(FXMLTableController.class.getResource(path));
+        AnchorPane a = loader.load();
+
+        tableController = loader.getController();
+
+        anchorTable.getChildren().add(a);
+        AnchorPane.setTopAnchor(a,0.0);
+        AnchorPane.setBottomAnchor(a,0.0);
+        AnchorPane.setLeftAnchor(a,0.0);
+        AnchorPane.setRightAnchor(a,0.0);
+    }
+
+    public void initializePlayer() throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        String path = "/futAges/view/PlayerView.fxml";
+        loader.setLocation(FXMLPlayerViewController.class.getResource(path));
+        AnchorPane a = loader.load();
+
+        playerViewController = loader.getController();
+
+        anchorMediaPlayer.getChildren().add(a);
+        AnchorPane.setTopAnchor(a,0.0);
+        AnchorPane.setBottomAnchor(a,0.0);
+        AnchorPane.setLeftAnchor(a,0.0);
+        AnchorPane.setRightAnchor(a,0.0);
+
+    }
+
+    public void setController(MainDataController controller) {
+        this.controller = controller;
+        configureMediaPlayerStep();
+        configureMediaPlayer();
+    }
+
+    private void configureMediaPlayerStep() {
+        playerViewController.setStep(controller.getTempoDivisao());
+        playerViewController.setStepListener((observable, oldValue, newValue) -> {
+            int value = StringFuncions.stringToInt(newValue);
+            controller.setTempo(value);
+        });
+    }
+
+    private void configureMediaPlayer() {
+        playerViewController.closePlayer();
+        playerViewController.setMediaPlayer(controller.getVideoPath());
+        playerViewController.setMediaPlayerListener((observable, oldValue, newValue) -> {
+        });
+    }
+}
