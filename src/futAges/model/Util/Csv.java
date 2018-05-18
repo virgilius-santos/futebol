88ea63a5b1e5d319aeb0b129aab400689b1368e4
Virgilius Santos <virgilius.santos@acad.pucrs.br>
2018-05-18 05:37:23 -0300

    add sort

--- src/futAges/model/Util/Csv.java
+++ src/futAges/model/Util/Csv.java
@@ -2,27 +2,34 @@ package futAges.model.Util;
 
 import futAges.model.Entity.FrameData;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Csv {
 
-    private static StringBuilder builder;
-    private static int countObj;
-    private static HashMap<Integer, Integer[]> linhas;
-    private static FrameData frameData;
-    private static String nome;
+    private static final String KEY_TEMPO = "Tempo;";
+    private static final String KEY_SEMI_COLON = ";";
+    private static final String KEY_BREAK_LINE = "\n";
+    private static final String KEY_EMPTY = "";
+
+    private Csv(){}
 
     public static String converter(List<FrameData> dados){
-        builder = new StringBuilder();
-        linhas = new HashMap<>();
-        countObj = dados.size();
-        builder.append("Tempo;");
+        StringBuilder builder = new StringBuilder();
+        HashMap<Integer, Integer[]> linhas = new HashMap<>();
+        int countObj = dados.size();
+
+        FrameData frameData;
+        String nome;
+
+        builder.append(KEY_TEMPO);
 
         for(int id=0; id<countObj; id++){
             frameData = dados.get(id);
             nome = frameData.getName();
-            builder.append(nome).append(";");
+            builder.append(nome).append(KEY_SEMI_COLON);
 
             int finalId = id;
             frameData.getQuadrants().forEach((tempo,quadrante) -> {
@@ -33,14 +40,23 @@ public class Csv {
             });
 
         }
+        builder.append(KEY_BREAK_LINE);
 
-        builder.append("\n");
+        linhas
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .forEach(set -> {
 
-        linhas.forEach((k, v) -> {
-            builder.append(k.toString()).append(";");
-            for (int i=0; i<v.length; i++)
-                builder.append( (v[i]==null) ? "" : v[i].toString()).append(";");
-            builder.append("\n");
+            builder.append(set.getKey()).append(KEY_SEMI_COLON);
+
+            for (Integer celula : set.getValue()) {
+                builder.append((celula == null) ? KEY_EMPTY : celula.toString());
+                builder.append(KEY_SEMI_COLON);
+            }
+
+            builder.append(KEY_BREAK_LINE);
+
         });
 
         return builder.toString();
