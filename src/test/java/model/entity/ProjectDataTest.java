1d985ea6de6a283d95e7de6e4f460216b46c4ccb
Virgilius Santos <virgilius.santos@acad.pucrs.br>
2018-05-31 13:58:55 -0300

    update import

--- src/test/java/model/entity/ProjectDataTest.java
+++ src/test/java/model/entity/ProjectDataTest.java
@@ -8,7 +8,8 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class ProjectDataTest {
 
