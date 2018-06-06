0447a9bded584f1c45ff7166d7555a8a64d502c3
Uriel Battanoli <urielbattanoli@gmail.com>
2018-06-05 22:49:07 -0300

    S10-T06, S10-T07: Campinho gerando linhas dinamicamente quando altera o campo de linha ou colunas

--- src/main/java/controller/FXMLCampinhoController.java
+++ src/main/java/controller/FXMLCampinhoController.java
@@ -1,7 +1,9 @@
 package controller;
 
 
+import javafx.scene.Node;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 import model.util.Validation;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,45 +19,61 @@ import java.util.ResourceBundle;
 public class FXMLCampinhoController implements Initializable {
 
     @FXML
-    private javafx.scene.image.ImageView soccerField;
-    @FXML
     private TextField nbRowsField;
     @FXML
     private TextField nbColumnsField;
     @FXML
     private GridPane gridPane;
-    @FXML
-    private javafx.scene.control.Label rows;
-    @FXML
-    private javafx.scene.control.Label columns;
-    @FXML
-    private javafx.scene.image.ImageView SoccerField;
 
-    private int linhas = 10;
-    int colunas = 10;
-
-
+    private int linesQnt = 0;
+    private int columnsQnt = 0;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        JvmMemManagerEntryMeta tilePane;
+        nbRowsField.textProperty().addListener((observable, oldValue, newValue) -> setLinesQuantity(newValue));
+        nbColumnsField.textProperty().addListener((observable, oldValue, newValue) -> setColumnsQuantity(newValue));
+    }
 
+    private void clearGridPane() {
+        while (gridPane.getRowConstraints().size() > 0) {
+            gridPane.getRowConstraints().remove(0);
+        }
 
+        while (gridPane.getColumnConstraints().size() > 0) {
+            gridPane.getColumnConstraints().remove(0);
+        }
+    }
 
-        gridPane.resize(511, 323);
-        for (int i = 0; i < linhas; i++) {
+    private void addGridRowsAndColums() {
+        for (int i = 0; i < linesQnt ; i++) {
             RowConstraints rConstraint = new RowConstraints();
-            rConstraint.setPercentHeight(100 / linhas);
+            rConstraint.setPrefHeight(gridPane.getHeight() / linesQnt);
+            rConstraint.setPercentHeight(100);
             gridPane.getRowConstraints().add(rConstraint);
         }
 
-        for (int i = 0; i < colunas; i++) {
+        for (int i = 0; i < columnsQnt; i++) {
             ColumnConstraints cConstraint = new ColumnConstraints();
-            cConstraint.setPercentWidth(100 / colunas);
+            cConstraint.setPrefWidth(gridPane.getHeight() / columnsQnt);
+            cConstraint.setPercentWidth(100);
             gridPane.getColumnConstraints().add(cConstraint);
         }
-
     }
+
+    private void setLinesQuantity(String text) {
+        if (text.isEmpty()) { return; }
+        linesQnt = Integer.parseInt(text);
+        clearGridPane();
+        addGridRowsAndColums();
+    }
+
+    private void setColumnsQuantity(String text) {
+        if (text.isEmpty()) { return; }
+        columnsQnt = Integer.parseInt(text);
+        clearGridPane();
+        addGridRowsAndColums();
+    }
+
     @FXML
     private void textFieldOnKeyPressed(KeyEvent event) {
         Validation.onKeyPressed(event);
@@ -65,8 +83,4 @@ public class FXMLCampinhoController implements Initializable {
     private void textFieldKeyTyped(KeyEvent event) {
         Validation.onKeyTyped(event);
     }
-
-
-
 }
-
