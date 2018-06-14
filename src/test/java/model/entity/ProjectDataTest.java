35f625348ec1b81c187390bfd6b117dc6f83476d
rafael.ritter <rafael.barni@acad.pucrs.br>
2018-06-14 16:58:30 -0300

    @rafael.ritter
    Foram documentados os testes da classe ProjectDataTest com suas devidas descrições.

--- src/test/java/model/entity/ProjectDataTest.java
+++ src/test/java/model/entity/ProjectDataTest.java
@@ -25,12 +25,20 @@ public class ProjectDataTest {
         projectData.setTempoDivisao(2);
     }
 
+    /**
+     * Testa se o arquivo de vídeo inicialmente atribuído ao objeto corresponde ao arquivo que
+     * realmente foi guardado e que está sendo retornado.
+     */
     @org.junit.Test
     public void getVideoFile() {
         File file = new File("VideoA.mp4");
         assertEquals(new File(file.toURI()), projectData.getVideoFile());
     }
 
+    /**
+     * Testa se a reatribuição do arquivo de vídeo do objeto realmente o atualiza e retorna
+     * o arquivo correto.
+     */
     @org.junit.Test
     public void setVideoFile() {
         File file = new File("VideoB.mp4");
@@ -38,12 +46,19 @@ public class ProjectDataTest {
         assertEquals(new File(file.toURI()), projectData.getVideoFile());
     }
 
+    /**
+     * Testa se o MD5 do vídeo inicialmente atribuído ao objeto corresponde ao MD5 que realmente
+     * foi guardado e que está sendo retornado.
+     */
     @org.junit.Test
     public void getVideoMD5() throws Exception {
         String md5 = Conversion.getMD5CheckSum(projectData.getVideoFile());
         assertEquals(md5, projectData.getVideoMD5());
     }
 
+    /**
+     * Testa se a reatribuição do MD5 do vídeo do objeto realmente o atualiza e retorna o MD5 correto.
+     */
     @org.junit.Test
     public void setVideoMD5() throws Exception {
         String md5 = Conversion.getMD5CheckSum(projectData.getVideoFile());
@@ -52,34 +67,56 @@ public class ProjectDataTest {
 
     }
 
+    /**
+     * Testa se o número de colunas inicial e padrão do objeto corresponde ao número que realmente
+     * foi guardado e que está sendo retornado.
+     */
     @org.junit.Test
     public void getColunas() {
         assertEquals((Integer) 8, projectData.getColumns());
     }
 
+    /**
+     * Testa se a reatribuição do número de colunas do objeto realmente o atualiza e retorna o número correto.
+     */
     @org.junit.Test
     public void setColunas() {
         projectData.setColumns(9);
         assertEquals((Integer) 9, projectData.getColumns());
     }
 
+    /**
+     * Testa se o número de linhas inicial e padrão do objeto corresponde ao número que realmente
+     * foi guardado e que está sendo retornado.
+     */
     @org.junit.Test
     public void getLinhas() {
         assertEquals((Integer) 4, projectData.getLines());
     }
 
+    /**
+     * Testa se a reatribuição do número de linhas do objeto realmente o atualiza e retorna o número correto.
+     */
     @org.junit.Test
     public void setLinhas() {
         projectData.setLines(5);
         assertEquals((Integer) 5, projectData.getLines());
     }
 
+    /**
+     * Testa se o arquivo do projeto inicialmente atribuído ao objeto corresponde ao arquivo que
+     * realmente foi guardado e que está sendo retornado.
+     */
     @org.junit.Test
     public void getProjetoFile() {
         File file = new File("teste.json");
         assertEquals(file, projectData.getProjetoFile());
     }
 
+    /**
+     * Testa se a reatribuição do arquivo do projeto desse objeto realmente o atualiza e retorna
+     * o arquivo correto.
+     */
     @org.junit.Test
     public void setProjetoFile() {
         File file = new File("teste2.json");
@@ -88,28 +125,48 @@ public class ProjectDataTest {
         assertEquals(file, projectData.getProjetoFile());
     }
 
+    /**
+     * Testa se o intervalo de tempo entre os frames inicialmente atribuído ao objeto corresponde
+     * ao intervalo de tempo que realmente foi guardado e que está sendo retornado.
+     */
     @org.junit.Test
     public void getTempoDivisao() {
         assertEquals((Integer) 2, projectData.getTempoDivisao());
     }
 
+    /**
+     * Testa se a reatribuição do intervalo de tempo entre os frames do objeto realmente o atualiza
+     * e retorna o intervalo de tempo correto.
+     */
     @org.junit.Test
     public void setTempoDivisao() {
         projectData.setTempoDivisao(3);
         assertEquals((Integer) 3, projectData.getTempoDivisao());
     }
 
+    /**
+     * Testa se o URI do vídeo inicialmente atribuído ao objeto corresponde ao URI que
+     * realmente foi guardado e que está sendo retornado.
+     */
     @org.junit.Test
     public void getVideoURI() {
         URI videoURI = projectData.getVideoFile().toURI();
         assertEquals((videoURI).toString(), projectData.getVideoURI());
     }
 
+    /**
+     * Testa se a quantidade inicial e padrão de objetos do ProjectData corresponde à quantidade
+     * que realmente foi guardada e que está sendo retornada.
+     */
     @org.junit.Test
     public void getDataSize() {
         assertEquals( 2, projectData.getDataSize());
     }
 
+    /**
+     * Testa se os dados do ProjectData (lista de FrameDatas) inicial correspondem aos dados
+     * que realmente foram guardados e que estão sendo retornados.
+     */
     @org.junit.Test
     public void getDados() {
         ArrayList<FrameData> dados = new ArrayList<>(10);
@@ -123,6 +180,10 @@ public class ProjectDataTest {
         assertEquals(dados.get(1).getQuadrants().size(), actual.get(1).getQuadrants().size());
     }
 
+    /**
+     * Testa se um dado individual da lista de FrameDatas corresponde ao dado que realmente
+     * foi guardado e que está sendo retornado.
+     */
     @org.junit.Test
     public void getData() {
         FrameData frameData = new FrameData();
@@ -130,6 +191,10 @@ public class ProjectDataTest {
         assertEquals(frameData.getQuadrants().size(), projectData.getData(0).getQuadrants().size());
     }
 
+    /**
+     * Testa se adição de um novo objeto à lista de FrameDatas realmente atualiza a lista,
+     * guarda e retorna o objeto correto.
+     */
     @org.junit.Test
     public void addData() {
         FrameData frameData = new FrameData();
